948dbb05b3ebe13df2049418392b619864648cb8
byauricasa <72792382@continental.edu.pe>
2022-10-28 15:27:41 -0500

    Merging

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import HeaderNavBar from './Aside/HeaderNavBar'
 import FormCP from './Comp/Form/FormCP'
-import InformECA from './SensoresAmbientales/InformECA'
+import RegistroIncidente from './Incidentes/RegistroIncidente'
 
 const MainView = ({nombre}) => {
   return (
     <div className='flex-1'>
       <HeaderNavBar nombre={nombre}/>
-      <InformECA/>      
+      <RegistroIncidente/>      
     </div>
   )
 }
