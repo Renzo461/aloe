319a25e93ecd2ffdd63e633bdad2f984e607f1fa
Renzo <72679166@continental.edu.pe>
2022-10-28 01:27:44 -0500

    ROUTING

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AsideNavBar from './components/Aside/AsideNavBar';
-import MainView from './components/MainView';
+import HeaderNavBar from './components/Aside/HeaderNavBar'
+import EBMain from './components/Estaciones Base/EBMain';
+import InformECA from './components/SensoresAmbientales/InformECA'
 
-const App =()=> {
-  const name="laura"
+const App = () => {
+  const name = "laura"
   return (
-    <div className="App flex flex-nowrap min-h-screen">
-      <AsideNavBar nombre={name}/>
-      <MainView nombre={name}/>      
-    </div>
+    <BrowserRouter>
+      <div className="App flex flex-nowrap min-h-screen">
+        <AsideNavBar nombre={name} />
+        <div className='flex-1'>
+          <HeaderNavBar nombre={name} />
+          <Routes>
+            <Route path='/ambiente' element={<EBMain/>}/>
+            <Route path='/configuracion' element={<InformECA/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Aside/AsideNavBar.jsx
+++ src/components/Aside/AsideNavBar.jsx
@@ -6,47 +6,32 @@ import { AiFillCar, AiFillSetting } from 'react-icons/ai'
 import { BsHouseDoorFill } from 'react-icons/bs'
 import { useState } from "react";
 
-const AsideNavBar = ({nombre}) => {  
+const AsideNavBar = ({ nombre }) => {
   const [colapse, setcolapse] = useState(true)
 
-  function activeMenu(e) {
-    //SELECCIONO TODOS LOS MENUS
-    const menuList = document.querySelectorAll(".menu")
-
-    // SI LE HAGO CLICK AL H5
-    if (e.target.parentElement.classList.contains("menu")) {
-      menuList.forEach(element => element.classList.remove("active"));
-      e.target.parentElement.classList.add("active")
-    }
-    // SI LE HAGO CLICK AL CONTENEDOR
-    else if (e.target.classList.contains("menu")) {
-      menuList.forEach(element => element.classList.remove("active"));
-      e.target.classList.add("active")
-    }
-  }
-  function colapseMenu(e){
+  function colapseMenu(e) {
     setcolapse(!colapse)
-    if(colapse){
+    if (colapse) {
       e.target.parentElement.parentElement.classList.remove("w-14")
-      e.target.parentElement.parentElement.classList.add("w-96")      
+      e.target.parentElement.parentElement.classList.add("w-96")
     }
-    else{
+    else {
       e.target.parentElement.parentElement.classList.remove("w-96")
-      e.target.parentElement.parentElement.classList.add("w-14")      
+      e.target.parentElement.parentElement.classList.add("w-14")
     }
   }
 
   return (
-    <div className='w-14 overflow-hidden bgVerde min-h-screen nav' onClick={activeMenu}>
+    <div className='w-14 overflow-hidden bgVerde min-h-screen nav'>
       <div className='flex justify-end h-14 p-2  border-b border-gray-100'>
         <img src={menu} alt="menuIcon" className='menuColapse cursor-pointer w-10' onClick={colapseMenu} />
       </div>
       <AsideNavBarUser nombre={nombre}></AsideNavBarUser>
-      <AsideNavBarMenu icon={<FaEnvira/>} titulo="Ambiente" active={true}></AsideNavBarMenu>
-      <AsideNavBarMenu icon={<AiFillCar/>} titulo="Transporte"></AsideNavBarMenu>
-      <AsideNavBarMenu icon={<FaUsers/>} titulo="usuarios"></AsideNavBarMenu>
-      <AsideNavBarMenu icon={<BsHouseDoorFill/>} titulo="Municipalidad"></AsideNavBarMenu>
-      <AsideNavBarMenu icon={<AiFillSetting/>} titulo="Configuración"></AsideNavBarMenu>      
+      <AsideNavBarMenu icon={<FaEnvira />} titulo="Ambiente" nav="/ambiente"/>
+      <AsideNavBarMenu icon={<AiFillCar />} titulo="Transporte" nav="/transporte"/>
+      <AsideNavBarMenu icon={<FaUsers />} titulo="usuarios" nav="/usuario"/>
+      <AsideNavBarMenu icon={<BsHouseDoorFill />} titulo="Municipalidad" nav="/municipalidad"/>
+      <AsideNavBarMenu icon={<AiFillSetting />} titulo="Configuración" nav="/configuracion"/>
     </div>
   )
 }
